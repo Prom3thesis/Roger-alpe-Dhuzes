93e91ddb295b32c538a6b2cf93c4a72c90417617
Prome Theus <prom3thesis@gmail.com>
2026-05-18 13:35:02 +0200

    Fix Sanity Studio env configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,15 @@ import {structureTool} from "sanity/structure";
 import {setPublishedAtOnPublish} from "./sanity/documentActions/setPublishedAtOnPublish";
 import {schemaTypes} from "./sanity/schemaTypes";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "yourprojectid";
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+const projectId =
+  process.env.SANITY_STUDIO_PROJECT_ID ||
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  "yourprojectid";
+
+const dataset =
+  process.env.SANITY_STUDIO_DATASET ||
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  "production";
 
 export default defineConfig({
   name: "default",
